901efcc9db19bcc748264deace9f302d37f2fcba
fng97 <fng97@icloud.com>
2025-07-07 18:02:21 +0100

    Make external links open in a new tab

--- pandoc/fix-md-links.lua
+++ pandoc/fix-md-links.lua
@@ -1,13 +1,12 @@
 -- When referring to other pages in markdown we use relative file paths. When converting to HTML,
--- the extensions of those files must be changed from ".md" to ".html".
+-- the extensions of those files must be changed from ".md" to ".html". To keep navigation smooth,
+-- we'll add the "_self" attribute so that these links aren't opened in a new tab (the default
+-- behaviour for links as defined in the template).
 
 function Link(link)
-	local is_external = link.target:sub(1, 8) == "https://" or link.target:sub(1, 7) == "http://"
-	local is_mailto = link.target:sub(1, 7) == "mailto:"
-
-	if not is_external and not is_mailto then
+	if link.target:match("%.md$") then
 		link.target = link.target:gsub("%.md$", ".html")
+		link.attributes["target"] = "_self"
+		return link
 	end
-
-	return link
 end
