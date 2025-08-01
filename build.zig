8418335d53d018ffc0e5fa3f0166fa8b4af04514
fng97 <fng97@icloud.com>
2025-08-01 14:26:52 +0100

    Glob files with Zig walker instead of git ls-files

--- build.zig
+++ build.zig
@@ -14,43 +14,49 @@ pub fn build(b: *std.Build) !void {
         break :dependency dependency.path("bin/pandoc");
     };
 
-    // The output folder for the website content. The contents of this folder get copied to the
-    // installation directory.
-    const website = b.addWriteFiles();
+    const website = b.addWriteFiles(); // output folder for website content
     _ = website.addCopyFile(b.path("styles.css"), "styles.css"); // copy in the style file
 
     // This step generates the list of blog post links and injects it into the final "index.html".
-    // It takes an input "index.html" path (--index=...), an output "index.html" path
-    // (--output=...), and all of the blog post paths (multiple --blog-post=... arguments).
+    // It takes an input "index.html" path (--index), an output "index.html" path (--output), and
+    // all of the blog post paths (multiple --blog-post arguments).
     const blog_list_builder = b.addExecutable(.{
         .name = "blog_list_builder",
         .root_source_file = b.path("blog_list_builder.zig"),
         .target = b.graph.host,
     });
     const build_blog_list_step = b.addRunArtifact(blog_list_builder);
-    const output_index = build_blog_list_step.addPrefixedOutputFileArg("--output=", "index.html");
+    const updated_index = build_blog_list_step.addPrefixedOutputFileArg("--output=", "index.html");
 
-    _ = website.addCopyFile(output_index, "index.html"); // copy in the final "index.html"
+    _ = website.addCopyFile(updated_index, "index.html"); // copy in the final "index.html"
 
-    // NOTE: This only works for tracked files. Expect an error if a committed md file is deleted.
-    var lines = std.mem.tokenizeScalar(u8, b.run(&.{ "git", "ls-files", "content/*.md" }), '\n');
+    // Process website content. Non-Markdown files are copied directly to the output. HTML is
+    // generated from Markdown files using Pandoc.
+    var dir = try std.fs.cwd().openDir("content", .{ .iterate = true });
+    defer dir.close();
+    var walker = try dir.walk(b.allocator);
+    defer walker.deinit();
+    while (try walker.next()) |entry| {
+        if (entry.kind != .file) continue;
 
-    // This iterates over all of the Markdown files to add them to the build tree. A Pandoc step
-    // (generates HTML from the Markdown file) is created for each file. The index and blog post
-    // Markdown files also get passed as arguments to build_blog_list_step (single step). The
-    // generated HTML from all but the index file are copied directly to the output folder.
-    while (lines.next()) |file_path| { // e.g. "content/index.md"
-        // Map "content/*.md" paths to "*.html" (e.g. "content/index.md" to "index.html").
-        const html_path = b.fmt("{s}.html", .{file_path[8 .. file_path.len - 3]});
+        const filepath = b.path(b.pathJoin(&.{ "content", entry.path }));
+        const filename = entry.basename;
+
+        // Copy in non-Markdown files (assets).
+        if (!std.mem.endsWith(u8, entry.basename, ".md")) {
+            _ = website.addCopyFile(filepath, entry.path); // preserve file structure
+            continue;
+        }
 
         // Blog posts start with the publication date and have the format "YYYYMMDD-*.md": eight
         // digits followed by a "-".
-        const is_blog_post = html_path.len >= 9 and html_path[8] == '-' and all_numeric: {
-            for (html_path[0..8]) |c| if (!std.ascii.isDigit(c)) break :all_numeric false;
-            break :all_numeric true;
+        const is_blog_post = filename.len >= 9 and filename[8] == '-' and starts_with_8_digits: {
+            for (filename[0..8]) |c| if (!std.ascii.isDigit(c)) break :starts_with_8_digits false;
+            break :starts_with_8_digits true;
         };
 
-        const pandoc_step = std.Build.Step.Run.create(b, "run pandoc");
+        // Create a Pandoc step for each Markdown file to generate HTML from the Markdown.
+        const pandoc_step = std.Build.Step.Run.create(b, b.fmt("pandoc: {s}", .{filename}));
         pandoc_step.addFileArg(pandoc_exe);
         pandoc_step.addArgs(&.{
             "--from=markdown",
@@ -62,31 +68,29 @@ pub fn build(b: *std.Build) !void {
         pandoc_step.addPrefixedFileArg("--lua-filter=", b.path("pandoc/title-from-h1.lua"));
         pandoc_step.addPrefixedFileArg("--lua-filter=", b.path("pandoc/fix-md-links.lua"));
         if (is_blog_post) { // pass the publication date as metadata to Pandoc
-            const year = html_path[0..4]; // YYYY
-            const month = html_path[4..6]; // MM
-            const day = html_path[6..8]; // DD
+            const year = filename[0..4]; // YYYY
+            const month = filename[4..6]; // MM
+            const day = filename[6..8]; // DD
             pandoc_step.addArg(b.fmt("--metadata=date:{s}-{s}-{s}", .{ year, month, day }));
         }
-        pandoc_step.addFileArg(b.path(file_path));
-        const generated_html = pandoc_step.captureStdOut();
+        pandoc_step.addFileArg(filepath);
+        const generated_html_path = pandoc_step.captureStdOut();
 
-        if (std.mem.eql(u8, file_path, "content/index.md")) {
-            build_blog_list_step.addPrefixedFileArg("--index=", generated_html);
-            // This will be the input "index.html" to the "build_blog_list_step" so we don't save it
-            // to the website folder below like we do for all the other generated html files.
-            // "build_blog_list_step" is responsible for saving this file to website folder once it
-            // has injected the blog post list.
+        // Map "*.md" -> "*.html" (e.g. "index.md" -> "index.html").
+        const html_filename = b.fmt("{s}.html", .{filename[0 .. filename.len - ".md".len]});
+
+        if (std.mem.eql(u8, filename, "index.md")) {
+            // Don't copy in "index.html". This step will save the final "index.html".
+            build_blog_list_step.addPrefixedFileArg("--index=", generated_html_path);
             continue;
         }
 
-        // Pass all generated blog posts to the blog post list generation step so it can assemble
-        // the blog post list.
         if (is_blog_post) build_blog_list_step.addPrefixedFileArg(
-            b.fmt("--blog-post={s}:", .{html_path}),
-            generated_html,
+            b.fmt("--blog-post={s}:", .{html_filename}), // need the filename for the hyperlink
+            generated_html_path,
         ); // e.g. "--blog-post=20250705-i-like-coffee.html:{zig-cache-path}/stdout"
 
-        _ = website.addCopyFile(generated_html, html_path); // copy in generated HTML file
+        _ = website.addCopyFile(generated_html_path, html_filename); // copy in generated HTML
     }
 
     b.installDirectory(.{
